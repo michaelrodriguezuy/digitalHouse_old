348aa4f72f5de96847858a49efe44375d804f621
olimarteam <73653954+olimarteam@users.noreply.github.com>
2022-09-19 20:11:58 -0300

    exmanen final

--- Programacion_Imperativa/Algoritmos 4/16S.js
+++ Programacion_Imperativa/Algoritmos 4/16S.js
@@ -73,12 +73,34 @@ const invierteNumero = numero => {
     return resultado
 }
 let resultado = invierteNumero(12345)
-console.log(`El resultado es ${resultado}`)
+// console.log(`El resultado es ${resultado}`)
 
 /**
 *5) Escribir una función que reciba una array y solo imprima los valores que se repiten. Por ejemplo, dada la siguiente array e índice, la función imprimirá '6,23,33,100'. let array =
 [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
 */
 
+let arreglo = [3,6,6,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]
 
+const devuelveRepetidos = arr => {
+    let arrTemporal=[]
+    let arrRepetidos=[]
+    let bandera=0
+    
+    for (let i = 0; i < arr.length; i++) { //22
+        for (let j = 0; j <= arrTemporal.length; j++) {
+            
+            if (arr[i] == arrTemporal[j]) {
+                bandera=1
+            
+            }                 
+        }    
+        if (bandera == 0) {
+            arrRepetidos.push(arr[i])
+        }       
+    }
+    return arrRepetidos
+}
 
+let resultado2 = devuelveRepetidos(arreglo)
+console.log (resultado2)
